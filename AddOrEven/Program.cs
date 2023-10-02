e0814581923bc4d9759ae6d73e96cfdbe0926602
Fjodor Pasitsnik <fjodor.pasitsnik@tktk.ee>
2023-10-02 06:52:03 -0200

    add week3 3 code

---
+++ AddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+// kas kasutaja arv on paaris voi paaritu
+
+int result = userNum % 2;
+
+if(result != 0) // 1= ei ole vordne
+{
+    Console.WriteLine("userNum number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even");
+}
